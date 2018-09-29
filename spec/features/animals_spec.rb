4ee7519d7e405920c1843978034aeeb0eae5b419
jhon <jhone.cortesg@gmail.com>
2018-09-29 15:16:23 -0500

    up

--- spec/features/animals_spec.rb
+++ spec/features/animals_spec.rb
@@ -5,9 +5,9 @@ RSpec.feature "Animals", type: :feature, js: true do
     visit animals_path
     within("#new_animal") do
       fill_in 'animal_nombre', with: 'Topo'
-      fill_in 'animal_url_foto', with: 'https://disruptiveludens.files.wordpress.com/2016/04/fake-companies-list-announced-by-tcs-and-ibm-2015.png'
+      fill_in 'animal_url_foto', with: 'https://disruake-ibm-2015.png'
     end
     click_button 'Create Animal'
-    expect(page).to have_content 'Success'
+    expect(page).to have_content 'Topo'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 require 'capybara/poltergeist'
+require 'database_cleaner'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -26,5 +27,25 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   
   config.filter_rails_from_backtrace!
+ 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
+  config.before(:each, type: :feature) do
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+    unless driver_shares_db_connection_with_specs
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
 end
